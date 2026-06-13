9ae8348c184b69b0afd75b4fcb88d26cff8cbba0
Hari R <harikrish58027@gmail.com>
2026-06-13 16:46:06 +0530

    terraform Auto deploy using Github Actions

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+
+  backend "s3" {
+    bucket         = "hari-tf-state-25"
+    key            = "terraform-test/terraform.tfstate"
+    region         = "us-east-1"
+    use_lockfile   = "true"
+    encrypt        = "true"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "aws_region"  {
+    type = string
+    description = "use this for Aws Region"
+}
+
+variable "vpc_cidr"  {
+    type = string
+    description = "use this for VPC Networking"
+}
+
+variable "subnets_IP"  {
+    type = string
+    description = "use this for subnets IP"
+}
