19c3bd6948c76645c19bfcbb481f66ed948d17d7
Hari R <harikrish58027@gmail.com>
2026-07-19 08:24:45 +0530

    chnaged from varibales to variable

--- varibles.tf
+++ varibles.tf
@@ -1,25 +1,25 @@
-variables "aws_region"{
+variable "aws_region"{
     type = string 
     descripton = "we used this Region for this Project"
 
 }
 
-variables "vpc_cidr"{
+variable "vpc_cidr"{
       type = string 
     descripton = "we used this CIDR block in this VPC "
 }
 
-variables "subnet_cidr"{
+variable "subnet_cidr"{
       type = string 
     descripton = "we used this CIDR in subnet"
 }
 
-variables "bucket_name"{
+variable "bucket_name"{
       type = string 
       descripton = "we used this bucket name for s3"
 }
 
-variables "availability_zones" {
+variable "availability_zones" {
     type = list(string)
     description = ""List of target availability zones for subnets"" 
 }
