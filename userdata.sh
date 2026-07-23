4f472f966f88eb2736101583d8a773779b982e0c
Hari R <harikrish58027@gmail.com>
2026-07-23 07:55:04 +0530

    Added bash in userdata

--- userdata.sh
+++ userdata.sh
@@ -1,16 +1,13 @@
-apt update 
+#!/bin/bash
 
-apt install -y nginx docker.io 
+apt update
+apt install -y nginx docker.io
 
 systemctl enable nginx
-
 systemctl start nginx
 
 systemctl enable docker
-
 systemctl start docker
 
 docker pull harikrishdocker25/test-app
-
 docker run -d --restart always -p 5000:3000 harikrishdocker25/test-app
-
