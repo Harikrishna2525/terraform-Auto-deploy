3b87679a45410086b1a96bf4514b3b049f1ede0d
Hari R <harikrish58027@gmail.com>
2026-07-23 07:59:32 +0530

    Added Sudo

--- userdata.sh
+++ userdata.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-apt update
-apt install -y nginx docker.io
+sudo apt update
+sudo apt install -y nginx docker.io
 
-systemctl enable nginx
-systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
 
-systemctl enable docker
-systemctl start docker
+sudo systemctl enable docker
+sudo systemctl start docker
 
-docker pull harikrishdocker25/test-app
-docker run -d --restart always -p 5000:3000 harikrishdocker25/test-app
+ docker pull harikrishdocker25/test-app
+ docker run -d --restart always -p 5000:3000 harikrishdocker25/test-app
