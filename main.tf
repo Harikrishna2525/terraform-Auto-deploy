00627619f5ad5998bfa6c8f1f88ebe20ea70cf56
Hari R <harikrish58027@gmail.com>
2026-07-23 07:46:41 +0530

    Added userdata to install nginx and docker directlyin Ec2 insated of manual

--- main.tf
+++ main.tf
@@ -141,11 +141,6 @@ resource "aws_instance" "web" {
   tags = { Name = "Web-Server" }
 }
 
-resource "aws_instance" "web" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public_1.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-}
+
 
 
