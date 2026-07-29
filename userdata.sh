7e05dc92ead7ff247df95694afa16a1cc2bfc41a
Hari R <harikrish58027@gmail.com>
2026-07-29 07:05:41 +0530

    Revert "Change the port in docker"
    This reverts commit e3f2f6e1a5bac1ad885384b5535bc5596f5b2341.

--- userdata.sh
+++ userdata.sh
@@ -10,4 +10,5 @@ sudo systemctl enable docker
 sudo systemctl start docker
 
  docker pull harikrishdocker25/k8s-app:v1.0
- docker run -d --restart always -p 5000:3000 harikrishdocker25/k8s-app:v1.0
+ docker run -d --restart always -p 3000:3000 harikrishdocker25/k8s-app:v1.0
+ 
