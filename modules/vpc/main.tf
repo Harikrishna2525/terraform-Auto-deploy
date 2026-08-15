70f574a3155c1d953c74bf08fb92790617db7ffc
Hari R <harikrish58027@gmail.com>
2026-08-15 10:27:05 +0530

    Added permissions

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -50,7 +50,6 @@ resource "aws_route_table" "public" {
 # Associate Route Table with Subnets
 resource "aws_route_table_association" "public" {
   count = length(var.subnet_cidrs)
-
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
